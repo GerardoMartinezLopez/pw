7b310256751e8c0ddde62347fe3bf3858c2141e3
GerardoMartinezLopez <95319538+GerardoMartinezLopez@users.noreply.github.com>
2021-11-30 19:44:15 -0600

    Initial commit

---
+++ examen3.js
@@ -0,0 +1,30 @@
+let botonagregar = document.querySelector('.botonagregar');
+let botoneliminar = document.querySelector('.botoneliminar');
+
+botonagregar.addEventListener('click', agregar);
+botoneliminar.addEventListener('click', agregar);
+function agregar() {
+    let rectangulo1 = document.querySelector('.rectangulo1');
+}
+function eliminar() {
+    alert('Desea eliminar');
+}
+let parra = document.querySelector('#pruebatexto');
+parra.addEventListener('mouseover', cambiar);
+
+function cambiar() {
+    let imagendeadpool = document.querySelector('.imag');
+    imagendeadpool.src = 'avengers.jpg';
+}
+
+function img1() {
+    prompt('Cual es tu pelicula?');
+}
+
+
+function clasificacion() {
+    prompt('Clasificacion');
+}
+
+
+
